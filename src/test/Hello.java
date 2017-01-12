bd2768931225275b04bb0f08711d3bbc3faa3144
dashagua6 <yhhcheng@gmail.com>
2017-01-12 22:33:14 +0800

    xiaogai

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println(7);
 		System.out.println(8);
+		System.out.println(8);
+		System.out.println(8);
+		System.out.println(8);
 	}
 }
